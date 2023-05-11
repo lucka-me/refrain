a9dd634c3f1e88050290149258aa8715c1e99ca9
lucka-me <i@lucka.moe>
2023-05-11 21:35:25 +0800

    Improve Output Path card - Unescape the displayed path - Add an Open button
    Signed-off-by: lucka-me <i@lucka.moe>

--- app/src/main/java/labs/lucka/refrain/ui/card/OutputPathCard.kt
+++ app/src/main/java/labs/lucka/refrain/ui/card/OutputPathCard.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
@@ -20,10 +21,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.documentfile.provider.DocumentFile
 import labs.lucka.refrain.R
 import labs.lucka.refrain.common.preferences.Keys
 import labs.lucka.refrain.ui.compose.Label
 import labs.lucka.refrain.ui.compose.rememberPreference
+import java.net.URLDecoder
 
 @Composable
 fun OutputPathCard(mutable: Boolean) {
@@ -48,12 +51,30 @@ fun OutputPathCard(mutable: Boolean) {
                 Icons.Filled.Folder,
                 style = MaterialTheme.typography.titleLarge
             )
-            Text(text = path.ifEmpty { stringResource(R.string.output_path_description) })
-            if (mutable) {
-                TextButton(onClick = { documentTreeLauncher.launch(Uri.parse(path)) }) {
-                    Text(
-                        stringResource(if (path.isEmpty()) R.string.output_path_select else R.string.output_path_change)
-                    )
+            if (path.isEmpty()) {
+                Text(text = stringResource(R.string.output_path_description))
+            } else {
+                Text(text = URLDecoder.decode(path, "UTF-8"))
+            }
+            Row {
+                if (path.isNotEmpty()) {
+                    TextButton(
+                        onClick = {
+                            val tree = DocumentFile.fromTreeUri(context, Uri.parse(path)) ?: return@TextButton
+                            val intent = Intent(Intent.ACTION_VIEW, tree.uri)
+                                .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                            context.startActivity(intent)
+                        }
+                    ) {
+                        Text(stringResource(R.string.output_path_open))
+                    }
+                }
+                if (mutable) {
+                    TextButton(onClick = { documentTreeLauncher.launch(Uri.parse(path)) }) {
+                        Text(
+                            stringResource(if (path.isEmpty()) R.string.output_path_select else R.string.output_path_change)
+                        )
+                    }
                 }
             }
         }
